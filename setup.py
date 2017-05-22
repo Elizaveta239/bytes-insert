9afcc53d472752c4b1a240646a8f497b08e34e21
Elizaveta Shashkova <Elizaveta239@yandex.ru>
2017-05-22 15:57:30 -0700

    Fix typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='bytesinsert',
     version='0.1.0',
     packages=['bytesinsert', 'bytesinsert.tests'],
-    url='https://github.com/Elizaveta239/bytes-insert.git',
+    url='https://github.com/Elizaveta239/bytes-insert',
     license='MIT',
     author='Elizaveta Shashkova',
     author_email='elizabeth.shashkova@gmail.com',
