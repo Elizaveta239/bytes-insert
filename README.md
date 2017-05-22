aaec640facd2d87cc8e9de148f2e66e39c0be185
Elizaveta Shashkova <Elizaveta239@yandex.ru>
2017-05-22 15:12:49 -0700

    Add setup.py and update readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# bytes-insert
+bytesinsert
+===========
+
 A library for inserting a piece of code into another piece of code
+
+Description
+-----------
+
+A library allows to insert one piece of code into another piece of code secretly, without changing line numbers.
+
+The current version supports only **Python 3.6**. Also it allows to insert only functions without arguments.
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import sys
+
+if not (sys.version_info[0] == 3 and sys.version_info[1] >= 6):
+    sys.exit('The library supports only Python 3.6')
+
+setup(
+    name='bytesinsert',
+    version='0.1.0',
+    packages=['bytesinsert', 'bytesinsert.tests'],
+    url='',
+    license='MIT',
+    author='Elizaveta Shashkova',
+    author_email='elizabeth.shashkova@gmail.com',
+    description='A library for inserting a piece of code into another piece of code'
+)
